9e9362f7c437493e8eb5fc49d1c177c7942c4d2a
DmitriySh <dashishmakov@gmail.com>
2017-08-06 00:25:00 +0300

    Changed income parameters

--- scripts/start_elevator.sh
+++ scripts/start_elevator.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -jar -Dfile.encoding=UTF-8 ../build/libs/elevator-simulator-all-*.jar -n 10 -h 5 -v 2 -d 20
+java -jar -Dfile.encoding=UTF-8 ../build/libs/elevator-simulator-all-*.jar -n 15 -h 4 -v 2 -d 5
