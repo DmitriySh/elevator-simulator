4dc95c7136be2d2919cae8c10a0cb9e6ff370623
DmitriySh <dashishmakov@gmail.com>
2017-08-06 22:14:35 +0300

    Was edit test method 'ElevatorConfigTest#configShouldHasValues'

--- src/test/java/ru/shishmakov/config/ElevatorConfigTest.java
+++ src/test/java/ru/shishmakov/config/ElevatorConfigTest.java
@@ -4,6 +4,7 @@ import org.aeonbits.owner.ConfigFactory;
 import org.junit.Test;
 import ru.shishmakov.BaseTest;
 
+import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 public class ElevatorConfigTest extends BaseTest {
@@ -12,6 +13,7 @@ public class ElevatorConfigTest extends BaseTest {
     public void configShouldHasValues() {
         ElevatorConfig config = ConfigFactory.create(ElevatorConfig.class);
 
+        assertNotNull("Object should not be null", config);
         assertTrue("Elevator interval value should be positive", config.elevatorIntervalMs() > 0);
         assertTrue("Main interval value should be positive", config.mainIntervalMs() > 0);
         assertTrue("Read delay value should be positive", config.queueReadDelay() > 0);
